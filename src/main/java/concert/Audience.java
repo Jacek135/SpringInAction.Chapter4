4a74196f50bdd459126d0976491b3d5c40ace201
Jacek135 <jac.poczatek@gmail.com>
2016-09-25 13:03:08 +0200

    Refactoring Audience with @Around annoatation

--- src/main/java/concert/Audience.java
+++ src/main/java/concert/Audience.java
@@ -1,5 +1,6 @@
 package concert;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 
 @Aspect
@@ -8,24 +9,16 @@ public class Audience {
     @Pointcut("execution(** concert.Performance.perform(..))")
     public void performance(){}
 
-    @Before("performance()")
-    public void silenceCellPhones() {
-        System.out.println("Silencing cell phones");
+    @Around("performance()")
+    public void watchPerformance(ProceedingJoinPoint jp){
+        try
+        {
+            System.out.println("Silencing cell phones");
+            System.out.println("Taking seats");
+            jp.proceed();
+            System.out.println("CLAP CLAP CLAP !!!");
+        } catch (Throwable e) {
+            System.out.println("Demanding a refund");
+        }
     }
-
-    @Before("performance()")
-    public void takeSeats() {
-        System.out.println("Taking seats");
-    }
-
-    @AfterReturning("performance()")
-    public void applause() {
-        System.out.println("CLAP CLAP CLAP");
-    }
-
-    @AfterThrowing("performance()")
-    public void demandRefund() {
-        System.out.println("Demanding a refund");
-    }
-
 }
